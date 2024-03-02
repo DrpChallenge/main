7ba2b37cc5afedabe746cbb042581dc4babd50e6
Shiyao DING <dingshiyao0217@gmail.com>
2024-03-02 16:04:39 +0900

    Add files via upload

--- policy/policy.py
+++ policy/policy.py
@@ -2,7 +2,7 @@ import gym
 import random
 
 ### submission information ####
-TEAM_NAME = "KunwooLee" 
+TEAM_NAME = "your_team_name" 
 #TEAM_NAME must be the same as the name registered on the DRP website 
 #(or the team name if participating as a team).
 ##############################
@@ -10,6 +10,7 @@ TEAM_NAME = "KunwooLee"
 def policy(obs, env): #Random Policy 
     actions = []
     for agi in range(env.agent_num):
+        #get_avail_agent_actions is used to get all the availble actions at current step
         _, avail_actions = env.get_avail_agent_actions(agi,env.n_actions)
         actions.append(random.choice(avail_actions))
 
