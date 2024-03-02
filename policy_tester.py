6d06c400a63de9378199219612abe6867b1a3ca7
Shiyao DING <dingshiyao0217@gmail.com>
2024-03-02 16:04:07 +0900

    Add files via upload

--- policy_tester.py
+++ policy_tester.py
@@ -1,19 +1,7 @@
 import gym
 import policy.policy as policy
 
-def policy_test(policy, agent_num, map_name, reward_list, start, goal, render):
-    """This function tests policy function.  
-
-    Args:
-        policy : policy function
-        agent_num(int): set agent number(min 2 max 30)
-        map_name (str): set map_name ("map_3x3","map_aoba01","map_osaka"is available)
-        reward_list (dict): set reward list
-        start (list): set start node
-        goal (list): set  goal node
-        render (bool): whether visualize environment state
-
-    """
+def policy_evaluation(policy, agent_num, map_name, reward_list, start, goal, render):
     assert agent_num == len(start) and agent_num == len(goal),"The number of elements in start and goal list does not match with agent_num."
     assert not any(element in start for element in goal),"The elements of goal and start must not match."
     env = gym.make(
@@ -24,30 +12,40 @@ def policy_test(policy, agent_num, map_name, reward_list, start, goal, render):
         start_ori_array=start,
     )
     obs = env.reset()
-    print("obs",obs,type(obs),)
-    print("action_space", env.action_space)
-    print("observation_space", env.observation_space)
+    print(f"observation_space:{env.observation_space}")
+    print(f"action_space:{env.action_space}")
+    
     done_all = False
     while not done_all:
         if render == True:
-            env.render()  # optional
+            env.render() 
+        print(f"obs:{obs}")# current global observation
         actions = policy(obs, env)  # policy:input n_obs,env return each agent's action
-        obs, reward, done, info = env.step(actions)
-        print("obs", obs)
-        print("actions", actions, "reward", reward, "done", done)
-        print("info", info)
+        obs, reward, done, info = env.step(actions) # transfer to next state once joint action is taken
+        print(f"obs:{obs}, actions:{actions}, reward:{reward}, done:{done},info:{info}")
         done_all = all(done)
         env.get_obs()
     
 if __name__ == "__main__":
-    agent_num = 3
-    map_name = "map_aoba01"
-    reward_list = {"goal": 100, "collision": -10, "wait": -10, "move": -1}
-    goal = [3, 6,1]
-    start = [0, 2,4]
-    render = True
-    policy_test(
-        policy=policy.policy,
+    agent_num = 3 # the number of drones (min:2 max:30)
+    map_name = "map_aoba01" # the map name (available maps: "map_3x3","map_aoba01","map_osaka" )
+    
+    # reward_list is individual reward function where 
+    # "goal: 100" means one drone will obtain 100 rewards once it reach its goal.
+    # Similarly, "collision"/"wait"/"move" are rewards when a collision happens/one drone wait one step/moves one step;
+    reward_list = {"goal": 100, "collision": -10, "wait": -10, "move": -1} # Developers can freely to alter the reward function (rewards are not used as evaluation index)
+    
+    start = [0,2,4] # drone1's start: node 0;  drone2's start: node 2;  drone3's start: node 4;
+    goal = [3,6,1] # drone1's goal: node 3;  drone2's goal: node 6;  drone3's goal: node 1;
+    render = True # Choose whether to visualize
+    
+    """
+    policy_evaluation() function is used to evaluate the "policy" developed by participants
+    participants are expected to develop "policy",
+    which is essentially a mapping from input(global observation) to output(joint action) at each step
+    """    
+    policy_evaluation(
+        policy=policy.policy, # this is an example policy 
         agent_num=agent_num,
         map_name=map_name,
         reward_list=reward_list,
